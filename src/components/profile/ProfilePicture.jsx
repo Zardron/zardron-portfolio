9feef1413d4b84b695b0af858ea7989c2e5f1602
Zardron Angelo G. Pesquera <zardron.pesquera1993@gmail.com>
2026-01-08 12:09:56 +0800

    Update profile picture to IMG_1444.jpg

--- src/components/profile/ProfilePicture.jsx
+++ src/components/profile/ProfilePicture.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import PROFILE_PIC from "../../assets/profile_pic.png";
 
 const ProfilePicture = () => {
   return (
     <div className="absolute -top-16 sm:-top-20 right-1/2 translate-x-1/2">
       <div className="dark:bg-black/80 w-32 sm:w-40 overflow-hidden border-4 border-white/80 dark:border-[#ffffff4d] rounded-md">
         <img
-          src={PROFILE_PIC}
+          src="/IMG_1444.jpg"
           alt="Profile Picture"
           className="w-full h-full object-cover hover:scale-110 transition-all ease-in-out duration-300"
         />
